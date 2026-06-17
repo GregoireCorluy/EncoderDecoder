981e4414cd0ba835089b161e37448284e9a9be6b
GregoireCorluy <gcorluy@gmail.com>
2026-06-17 17:16:33 +0200

    set back old bias and scaling

--- src/EncoderDecoder/utils.py
+++ src/EncoderDecoder/utils.py
@@ -194,7 +194,9 @@ class loadData:
         list_species_input = self.metadata["list_species_input"]
         list_species_output = self.metadata["list_species_output_evaluation"]
         input_scaling_name = self.metadata["input_scaling_name"]
-        input_scaling, input_bias = self.getInputScalingBias(path_data)
+        #input_scaling, input_bias = self.getInputScalingBias(path_data)
+        input_scaling = self.metadata["input_species_scaling"]
+        input_bias = self.metadata["input_species_bias"]
         temperature_at_output = self.metadata["temperature_output"]
         header = "infer"
         extra_manifold_variables = self.metadata["extra_manifold_parameters"]
@@ -223,9 +225,9 @@ class loadData:
 
         model = self.loadModel(self.filename_model)
 
-        input_species_scaling, _ = self.getInputScalingBias(path_data)
+        #input_species_scaling, _ = self.getInputScalingBias(path_data)
 
-        output_with_PVsource = model.get_source_PV(output, input_species_scaling)
+        output_with_PVsource = model.get_source_PV(output, self.metadata["input_species_scaling"])
 
         extraVars, PV, _ = model.get_extraVar_PV_PVsource(input, output)
 
